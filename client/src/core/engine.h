8c4a0ca26e73d66e075c781a58c33d9ad42c92d8
ViniAsDeveloper <avude@avude.com>
2026-06-21 23:09:31 +1000

    Removed other changes to avoid conflicts

--- client/src/core/engine.h
+++ client/src/core/engine.h
@@ -4,22 +4,6 @@
 class Engine{
 private:
     bool m_isRunning = true;
-
-    static Engine* s_instance;
-
-    Engine(){   }
-
-public:
-    static Engine* getInstance() {
-        if (s_instance == nullptr) {
-            s_instance = new Engine();
-        }
-        return s_instance;
-    }
-
-    int avude = 0;
-
-    void run();
 };
 
 #endif // _ENGINE_H_
